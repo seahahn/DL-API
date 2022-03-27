b0f0b478b573a2b9bdffd04286381fd6283c7c21
seahahn <seah.ahn.nt@gmail.com>
2022-03-27 17:26:14 +0900

    deployment setting

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ app.add_middleware(
 )
 
 from db_api.api import (
-    create_post, update_post, delete_post, read_posts, read_posts_by_div, read_post
+    index, create_post, update_post, delete_post, read_posts, read_posts_by_div, read_post
 )
 
+index = app.get("/")(index)
 create_post = app.post("/posts/", response_model=schemas.AiPost)(create_post)
 update_post = app.put("/posts/{post_id}", response_model=schemas.AiPost)(update_post)
 delete_post = app.delete("/posts/{post_id}")(delete_post)
