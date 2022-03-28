8c03357f8682f16ce96e2e43888393d7d2946340
seahahn <seah.ahn.nt@gmail.com>
2022-03-29 07:40:44 +0900

    AI Post func updated

--- db_api/database.py
+++ db_api/database.py
@@ -10,6 +10,6 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 Base = declarative_base()
