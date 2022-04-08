1cd9c98fe9e9eeda2cb441806744335b6625950a
seahahn <seah.ahn.nt@gmail.com>
2022-04-09 00:48:20 +0900

    README contents upgraded

--- StarGAN/hair_color_gen.py
+++ StarGAN/hair_color_gen.py
@@ -5,7 +5,8 @@ from fastapi.responses import StreamingResponse
 MIME_IMG = ["image/jpg", "image/jpeg", "image/png"]
 
 async def hair_color_gen(img: UploadFile):
-    """인물 이미지 넣으면 머리색 바꿔주는 딥 러닝 모델 API
+    """
+    인물 이미지 넣으면 머리색 바꿔주는 딥 러닝 모델 API
 
     Args:
         img (UploadFile): 입력된 인물 이미지 파일
